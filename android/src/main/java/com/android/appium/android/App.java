fd4f42b9d43864acd517d0aee52c1bab9515778a
rajendra8 <rajendra8.sahu@gmail.com>
2020-06-04 10:50:15 +0530

    Android New project

---
+++ android/src/main/java/com/android/appium/android/App.java
@@ -0,0 +1,13 @@
+package com.android.appium.android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
